232ae106056911bd415b621a824b0ddc18828eaa
Ken Umeda <kenumeda1@gmail.com>
2023-05-10 22:53:50 +1000

    updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @typ ne {import('next').NextConfig} */
 const nextConfig = {}
 
 module.exports = nextConfig
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>hello worls</div>;
+};
+
+export default page;
